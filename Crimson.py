5291ede180c4584d43c82fd01fc65a116edcf026
Luan Ferreira <fe.luan@protonmail.com>
2026-08-01 22:56:15 -0300

    fix: pequena correção na versão, para referenciar a versão atual da aplicação

--- Crimson.py
+++ Crimson.py
@@ -159,8 +159,8 @@ DICIONARIO_IDIOMAS = {
         "English":          "Interface Theme:"
     },
     "lbl_versao": {
-        "Portuguese":       "Versão atual: V.1.0.0",
-        "English": "        Current version: V.1.0.0"
+        "Portuguese":       "Versão atual: V.2.0.0",
+        "English": "        Current version: V.2.0.0"
     },
     "txt_disclaimer": {
         "Portuguese":       "Desenvolvido sem fins comerciais\nQualquer distribuição deve ser gratuita\ne livre para todos\n",
